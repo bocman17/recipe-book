9ae458fec0f316fb5486894a891cf414bab5427e
Tomas Hejlik <tomas.hejlik@plus4u.net>
2022-11-19 18:10:43 +0100

    new branch, new BE

--- src/app/services/data-storage.service.ts
+++ src/app/services/data-storage.service.ts
@@ -14,32 +14,25 @@ export class DataStorageService {
   storeRecipes() {
     const recipes = this.recipeService.getRecipes();
     this.http
-      .put(
-        'https://recipe-book-f4d18-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
-        recipes
-      )
+      .put('https://localhost:7082/api/recipe', recipes)
       .subscribe((response) => {
         console.log(response);
       });
   }
 
   fetchRecipes() {
-    return this.http
-      .get<Recipe[]>(
-        'https://recipe-book-f4d18-default-rtdb.europe-west1.firebasedatabase.app/recipes.json'
-      )
-      .pipe(
-        map((recipes) => {
-          return recipes.map((recipe) => {
-            return {
-              ...recipe,
-              ingredients: recipe.ingredients ? recipe.ingredients : [],
-            };
-          });
-        }),
-        tap((recipes) => {
-          this.recipeService.setRecipes(recipes);
-        })
-      );
+    return this.http.get<Recipe[]>('https://localhost:7082/api/recipe').pipe(
+      map((recipes) => {
+        return recipes.map((recipe) => {
+          return {
+            ...recipe,
+            ingredients: recipe.ingredients ? recipe.ingredients : [],
+          };
+        });
+      }),
+      tap((recipes) => {
+        this.recipeService.setRecipes(recipes);
+      })
+    );
   }
 }
